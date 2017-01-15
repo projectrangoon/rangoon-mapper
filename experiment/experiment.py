40a84ecda235232eaadde03826527a88d775760c
Kaung Htet Zaw <me@kaunghtet.xyz>
2017-01-15 19:54:33 +0800

    Update data and make unique stops list

--- experiment/experiment.py
+++ experiment/experiment.py
@@ -1,7 +1,8 @@
+from pprint import pprint
 import csv
 import json
 
-with open('bus_stops_by_service_000114.tsv') as f:
+with open('bus_stops_by_service_000115.tsv') as f:
     raw_data = csv.reader(f, delimiter='\t')
     bus_stops = []
     for row in raw_data:
@@ -19,6 +20,37 @@ with open('bus_stops_by_service_000114.tsv') as f:
             'lng': row[10]
         }
         bus_stops.append(stop)
+    bus_stops = bus_stops[1:]
 
-with open('bus_stops.json', 'wb') as f:
-    f.write(json.dumps(bus_stops[1:]))
+
+with open('all_bus_stops.json', 'wb') as f:
+    f.write(json.dumps(bus_stops))
+
+
+unique_stops = {}
+for bus in bus_stops:
+    if bus['bus_stop_id'] not in unique_stops:
+        unique_stops[bus['bus_stop_id']] = {
+            'name_en': bus['name_en'],
+            'name_mm': bus['name_mm'],
+            'road_en': bus['road_en'],
+            'road_mm': bus['road_mm'],
+            'township_en': bus['township_en'],
+            'township_mm': bus['township_mm'],
+            'lat': bus['lat'],
+            'lng': bus['lng'],
+            'services': [
+                {
+                    'service_name': bus['service_name'],
+                    'sequence': bus['sequence']
+                }
+            ]
+        }
+    else:
+        unique_stops[bus['bus_stop_id']]['services'].append({
+            'service_name': bus['service_name'],
+            'sequence': bus['sequence']
+        })
+
+with open('unique_stops.json', 'wb') as f:
+    f.write(json.dumps(unique_stops))
