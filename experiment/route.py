de73089de8a14704db0468ba5727cf6f00dcd01c
Kaung Htet Zaw <me@kaunghtet.xyz>
2017-01-29 00:19:04 +0800

    Generate graph from script side

--- experiment/route.py
+++ experiment/route.py
@@ -60,86 +60,91 @@ for no, stops in groups:
         current_index = stops.index(stop)
         key = stop['bus_stop_id']
         if key not in graph:
-            graph[key] = {}
+            graph[key] = list()
 
         current_stop = stop
         next_stop = stops[current_index + 1]
         distance = get_distance(float(current_stop['lng']), float(current_stop['lat']), float(next_stop['lng']), float(next_stop['lat']))
         assert distance >= 0, (current_stop, next_stop)
 
-        graph[key][(next_stop['bus_stop_id'], next_stop['service_name'])] = distance
-pprint(graph[1])
-pprint(graph[2])
-pprint(graph[3])
-pprint(graph[4])
-pprint(graph[5])
+        next_stop['distance'] = distance
+        graph[key].append(next_stop)
+        # graph[key][(next_stop['bus_stop_id'], next_stop['service_name'])] = distance
+
+for key, value in graph.iteritems():
+    graph[key] = dict((v['bus_stop_id'], v) for v in value).values()
+
+
+with open('adjancencyList.json', 'wb') as f:
+    f.write(json.dumps(graph))
+
 
 
 # print 'Breadth First Search'
-def bfs(graph, start, end):
-    from Queue import Queue
-    seen = set()
-    queue = Queue()
-    queue.put((0, [start]))
+# def bfs(graph, start, end):
+#     from Queue import Queue
+#     seen = set()
+#     queue = Queue()
+#     queue.put((0, [start]))
 
-    while queue:
-        (curr_distance, path) = queue.get()
-        node = path[-1]
-        if node == end:
-            return path
+#     while queue:
+#         (curr_distance, path) = queue.get()
+#         node = path[-1]
+#         if node == end:
+#             return path
 
-        if node in seen:
-            continue
-        seen.add(node)
+#         if node in seen:
+#             continue
+#         seen.add(node)
 
-        for adjacent, distance in graph.get(node, {}).items():
-            new_path = list(path)
-            new_path.append(adjacent)
-            queue.put(queue, (curr_distance, new_path))
-
-
-# path = bfs(graph, start, end)
-# pprint(map(get_name, path))
-
-print 'Dijkstra with (Per stop cost: %f, Transfer Cost: %f)' % (cost_per_stop, cost_per_transfer)
-def dijkstra(graph, start, end):
-    import heapq
-    seen = set()
-    queue = []
-
-    # put the first path into the queue
-    heapq.heappush(queue, (0, 0, 0, [(start, None)]))
-    while queue:
-        (curr_cost, curr_distance, curr_transfers, path) = heapq.heappop(queue)
+#         for adjacent, distance in graph.get(node, {}).items():
+#             new_path = list(path)
+#             new_path.append(adjacent)
+#             queue.put(queue, (curr_distance, new_path))
 
 
-        # get the last node from the path
-        (node, curr_service) = path[-1]
-        if node == end:
-            return (curr_cost, curr_distance, curr_transfers, path)
+# # path = bfs(graph, start, end)
+# # pprint(map(get_name, path))
 
-        if (node, curr_service) in seen:
-            continue
-        seen.add((node, curr_service))
+# print 'Dijkstra with (Per stop cost: %f, Transfer Cost: %f)' % (cost_per_stop, cost_per_transfer)
+# def dijkstra(graph, start, end):
+#     import heapq
+#     seen = set()
+#     queue = []
 
-        for (adjacent, service), distance in graph.get(node, {}).items():
-            new_path = list(path)
-            new_path.append((adjacent, service))
-            new_distance = curr_distance + distance
-            new_cost = distance + curr_cost
-            new_transfers = curr_transfers
-            if curr_service != service:
-                new_cost += cost_per_transfer
-                new_transfers += 1
-            new_cost += cost_per_stop
+#     # put the first path into the queue
+#     heapq.heappush(queue, (0, 0, 0, [(start, None)]))
+#     while queue:
+#         (curr_cost, curr_distance, curr_transfers, path) = heapq.heappop(queue)
 
-            heapq.heappush(queue, (new_cost, new_distance, new_transfers, new_path))
 
-(cost, distance, transfers, path) = dijkstra(graph, start, end)
+#         # get the last node from the path
+#         (node, curr_service) = path[-1]
+#         if node == end:
+#             return (curr_cost, curr_distance, curr_transfers, path)
 
-for bus_id, service in path:
-    print service, get_name(bus_id)
-print "cost: ", cost
-print "length: ", len(path)
-print "transfers: ", transfers
+#         if (node, curr_service) in seen:
+#             continue
+#         seen.add((node, curr_service))
+
+#         for (adjacent, service), distance in graph.get(node, {}).items():
+#             new_path = list(path)
+#             new_path.append((adjacent, service))
+#             new_distance = curr_distance + distance
+#             new_cost = distance + curr_cost
+#             new_transfers = curr_transfers
+#             if curr_service != service:
+#                 new_cost += cost_per_transfer
+#                 new_transfers += 1
+#             new_cost += cost_per_stop
+
+#             heapq.heappush(queue, (new_cost, new_distance, new_transfers, new_path))
+
+# (cost, distance, transfers, path) = dijkstra(graph, start, end)
+
+# for bus_id, service in path:
+#     print service, get_name(bus_id)
+# print "cost: ", cost
+# print "length: ", len(path)
+# print "transfers: ", transfers
 
