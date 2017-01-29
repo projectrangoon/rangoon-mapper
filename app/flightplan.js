0afb56d2e34a8199dcff1d9b78306bfcba8a7ea3
Kaung Htet Zaw <me@kaunghtet.xyz>
2017-01-29 23:59:59 +0800

    Add deployment scripts and CI config

---
+++ app/flightplan.js
@@ -0,0 +1,18 @@
+var plan = require('flightplan');
+
+plan.target('production', {
+  host: '188.166.226.121',
+  username: 'deploy',
+  privateKey: '../conf.d/ssh/deploy',
+  agent: process.env.SSH_AUTH_SOCK
+});
+
+plan.remote(function (remote) {
+  remote.with('cd /var/www/rangoon-mapper', function () {
+    remote.exec('git pull');
+    remote.with('cd app', function () {
+      remote.exec('npm install');
+      remote.exec('npm build');
+    });
+  });
+});
