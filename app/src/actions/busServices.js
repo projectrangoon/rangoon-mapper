a90c3685e878ca82a7b41a2345444ababdab9485
Kaung Htet Zaw <me@kaunghtet.xyz>
2017-01-31 18:04:37 +0800

    Make layout responsive

--- app/src/actions/busServices.js
+++ app/src/actions/busServices.js
@@ -1,7 +1,6 @@
 import types from '../constants/ActionTypes';
 
-export default const loadAllBusServices = (busServices) =>
-  {
-    type: types.LOAD_ALL_BUS_SERVICES,
+export default const loadAllBusServices = (busServices) => {
+  type: types.LOAD_ALL_BUS_SERVICES,
     busServices
-  }
+};
