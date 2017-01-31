df8aa4c47cb23e460d690a29d1eabaffd5ec64db
Min Thet Naing <me@minthetnaing.xyz>
2017-02-01 01:01:30 +0800

    Setup google analytics

--- app/src/index.js
+++ app/src/index.js
@@ -13,9 +13,19 @@ const store = globalStore();
 // Create an enhanced history that syncs navigation events with the store
 const history = syncHistoryWithStore(browserHistory, store);
 
+// Google analytics
+const ReactGA = require('react-ga');
+
+ReactGA.initialize('UA-75086119-2');
+
+const logPageView = () => {
+  ReactGA.set({ page: window.location.pathname });
+  ReactGA.pageview(window.location.pathname);
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <Router history={history} routes={routes} />
+    <Router history={history} routes={routes} onUpdate={logPageView} />
   </Provider>,
   document.getElementById('root'),
 );
