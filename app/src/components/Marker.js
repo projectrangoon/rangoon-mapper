267a327b513ae7c4213dd1f2e0506ce4ee4c86b8
Kaung Htet Zaw <me@kaunghtet.xyz>
2017-02-02 14:26:45 +0800

    Place bus stop markers on calculated route

--- app/src/components/Marker.js
+++ app/src/components/Marker.js
@@ -1,37 +1,10 @@
 import React from 'react';
+import pin from '../assets/pin.png';
 
-const K_WIDTH = 10;
-const K_HEIGHT = 10;
-
-const markerStyle = {
-  position: 'absolute',
-  width: K_WIDTH,
-  height: K_HEIGHT,
-  left: -K_WIDTH / 2,
-  top: -K_HEIGHT / 2,
-
-  border: '5px solid #f44336',
-  borderRadius: K_HEIGHT,
-  backgroundColor: 'white',
-  textAlign: 'center',
-  color: '#3f51b5',
-  fontSize: 16,
-  fontWeight: 'bold',
-  padding: 4,
-};
-
-const Marker = props => (
-  <div style={markerStyle}>
-    {props.text}
-  </div>
-  );
-
-Marker.defaultProps = {
-  text: null,
-};
-
-Marker.propTypes = {
-  text: React.PropTypes.string,
-};
+const Marker = () =>
+  <div
+    className="marker"
+    style={{ backgroundImage: pin }}
+  />;
 
 export default Marker;
--- app/src/reducers/map.js
+++ app/src/reducers/map.js
@@ -1,5 +1,5 @@
 import types from '../constants/ActionTypes';
-import { calculateRoute, getLatLng } from '../utils';
+import { calculateRoute } from '../utils';
 
 const initialState = {
   center: { lat: 16.7943528, lng: 96.1518985 },
@@ -33,7 +33,7 @@ const map = (state = initialState, action) => {
       const { graph, startStop, endStop } = action;
       const route = calculateRoute(graph, startStop, endStop);
       return Object.assign({}, state, {
-        routeMarkers: getLatLng(route),
+        routeMarkers: route,
       });
     }
     case types.AJACENCY_LIST_LOADED: {
