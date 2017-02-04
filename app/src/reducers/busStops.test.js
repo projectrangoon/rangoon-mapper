e764451cde8dad02415495001f390014da07fef9
Min Thet Naing <me@minthetnaing.xyz>
2017-02-04 19:47:01 +0800

    Revert "Add pre-commit to run lint and test"
    This reverts commit 44f5e3b5a48983cea1c4d49fa185874dd904e3b8.

--- app/src/reducers/busStops.test.js
+++ app/src/reducers/busStops.test.js
@@ -7,16 +7,12 @@ describe('Calculating Route', () => {
   it('Short straightline path', () => {
     const startStop = allBusStops[4524];
     const endStop = allBusStops[4527];
-    expect(calculateRoute(graph, startStop, endStop)).toEqual({
-      currCost: NaN,
-      currDistance: 1.3202002349059285,
-      currTransfers: 0,
-      path: [
-        startStop,
-        allBusStops[4525],
-        allBusStops[4526],
-        endStop,
-      ] });
+    expect(calculateRoute(graph, startStop, endStop)).toEqual([
+      startStop,
+      allBusStops[4525],
+      allBusStops[4526],
+      endStop,
+    ]);
   });
 
   // it('Long straightline path', () => {
