ceb34c28b456f0b4df4134894037902f705a6dda
Min Thet Naing <me@minthetnaing.xyz>
2017-02-27 13:44:43 +0800

    Fix polylines and refactor some map actions

--- app/src/actions/map.js
+++ app/src/actions/map.js
@@ -1,105 +1,121 @@
 import { push } from 'react-router-redux';
 import _ from 'lodash';
 import types from '../constants/ActionTypes';
-import { calculateRoute as calculateRouteAction } from '../utils';
+import { calculateRoute as calculateRouteAction, drawPolylines as drawPolylinesAction, createActions } from '../utils';
 
-export const handlePlacesChanged = places => ({
-  type: types.PLACES_CHANGED,
-  places,
-});
+// Begin Action creators
+// const handlePlacesChangedActions = createActions([
+//   types.PLACES_CHANGED_REQUEST,
+//   types.PLACES_CHANGED_SUCCESS,
+//   types.PLACES_CHANGED_FAIL,
+// ]);
+const updateMapCenterActions = createActions([
+  types.UPDATE_MAP_CENTER_REQUEST,
+  types.UPDATE_MAP_CENTER_SUCCESS,
+  types.UPDATE_MAP_CENTER_FAIL,
+]);
+const mapLoadActions = createActions([
+  types.MAP_LOAD_REQUEST,
+  types.MAP_LOAD_SUCCESS,
+  types.MAP_LOAD_FAIL,
+]);
+const loadAdjacencyListActions = createActions([
+  types.AJACENCY_LOAD_REQUEST,
+  types.AJACENCY_LOAD_SUCCESS,
+  types.AJACENCY_LOAD_FAIL,
+]);
+const calculateRouteActions = createActions([
+  types.CALCULATE_ROUTE_REQUEST,
+  types.CALCULATE_ROUTE_SUCCESS,
+  types.CALCULATE_ROUTE_ERROR,
+]);
+const drawPolylinesActions = createActions([
+  types.DRAW_POLYLINES_REQUEST,
+  types.DRAW_POLYLINES_SUCCESS,
+  types.DRAW_POLYLINES_FAIL,
+]);
+const selectStartStopActions = createActions([
+  types.SELECT_START_STOP_REQUEST,
+  types.SELECT_START_STOP_SUCCESS,
+  types.SELECT_START_STOP_FAIL,
+]);
+const selectEndStopActions = createActions([
+  types.SELECT_END_STOP_REQUEST,
+  types.SELECT_END_STOP_SUCCESS,
+  types.SELECT_END_STOP_FAIL,
+]);
+// End Action creators
 
-export const updateMapCenter = (google, routePath, startStop, endStop) => {
-  const bounds = new google.maps.LatLngBounds();
-  bounds.extend(startStop);
+// export const handlePlacesChanged = places => ({
+//   type: types.PLACES_CHANGED,
+//   places,
+// });
+export const updateMapCenter = () => (dispatch, getState) => {
+  const { map } = getState();
+  const { google, routePath } = map;
   if (routePath.path.length > 1) {
+    dispatch(updateMapCenterActions.request(map));
+    const bounds = new google.maps.LatLngBounds();
     routePath.path.forEach(stop => bounds.extend(stop));
-  }
-  bounds.extend(endStop);
-  google.map.fitBounds(bounds);
-  return { type: types.UPDATE_MAP_CENTER };
-};
-
-
-export const drawPolylines = (google, routePath, startStop, endStop) => {
-  const services = _.groupBy(routePath.path || [], 'service_name');
-  _.map(services, (service) => {
-    const polyline = new google.maps.Polyline({
-      strokeColor: service[0].color,
-      strokeOpacity: 1,
-      strokeWeight: 3,
-      clickable: false,
-      path: service,
-    });
-    polyline.setMap(google.map);
-  });
-
-  const lineSymbol = {
-    path: 'M 0,-1 0,1',
-    strokeOpacity: 0.5,
-    scale: 4,
-  };
-
-  if (startStop.bus_stop_id !== routePath.path[0].bus_stop_id) {
-    const polyline = new google.maps.Polyline({
-      strokeColor: '#ffffff',
-      strokeOpacity: 0,
-      strokeWeight: 2,
-      clickable: false,
-      icons: [{
-        icon: lineSymbol,
-        offset: '0',
-        repeat: '20px',
-      }],
-      path: [startStop, routePath.path[0]],
-    });
-    polyline.setMap(google.map);
-  }
-
-  if (endStop.bus_stop_id !== routePath.path[routePath.path.length - 1].bus_stop_id) {
-    const polyline = new google.maps.Polyline({
-      strokeColor: '#ffffff',
-      strokeOpacity: 0,
-      strokeWeight: 2,
-      clickable: false,
-      icons: [{
-        icon: lineSymbol,
-        offset: '0',
-        repeat: '20px',
-      }],
-      path: [endStop, routePath.path[routePath.path.length - 1]],
-    });
-    polyline.setMap(google.map);
+    google.map.fitBounds(bounds);
+    dispatch(updateMapCenterActions.success(map));
   }
 };
 
-export const calculateRoute = (graph, busStopsMap, startStop, endStop, google) =>
-  dispatch =>
-    calculateRouteAction(graph, busStopsMap, startStop, endStop).then(
-      (routePath) => {
-        if (google) {
-          updateMapCenter(google, routePath, startStop, endStop);
-          drawPolylines(google, routePath, startStop, endStop);
-          dispatch({ type: types.PLACE_MARKERS, routePath });
-          dispatch(push(`/directions/${startStop.bus_stop_id}/${endStop.bus_stop_id}`));
-        }
+export const drawPolylines = () =>
+  (dispatch, getState) => {
+    const { map } = getState();
+    const { google, routePath, startStop, endStop, polylines } = map;
+
+    // Clear all previous polylines from state if there are any
+    if (polylines) {
+      // can still put in actions if we would like to e.g (dispatch(removePolylines()))
+      _.map(polylines, (polyline) => {
+        polyline.setMap(null);
+      });
+    }
+
+    dispatch(drawPolylinesActions.request());
+
+    drawPolylinesAction(google, routePath, startStop, endStop).then(
+      (response) => {
+        dispatch(drawPolylinesActions.success({ polylines: response }));
       },
       (error) => {
-        dispatch({
-          type: types.CALCULATE_ROUTE_ERROR,
-        });
-        throw error;
+        dispatch(drawPolylinesActions.fail(error));
       },
     );
+  };
+
+export const calculateRoute = (startStop, endStop) =>
+  (dispatch, getState) => {
+    const { map } = getState();
+    const { graph, busStopsMap } = map;
+
+    dispatch(calculateRouteActions.request());
+
+    calculateRouteAction(graph, busStopsMap, startStop, endStop).then(
+      (routePath) => {
+        dispatch(calculateRouteActions.success({ routePath }));
+        dispatch(updateMapCenter());
+        dispatch(drawPolylines());
+        dispatch(push(`/directions/${startStop.bus_stop_id}/${endStop.bus_stop_id}`));
+      },
+      (error) => {
+        dispatch(calculateRouteActions.fail(error));
+      },
+    );
+  };
 
 export const onMapLoad = google =>
   (dispatch, getState) => {
     const { map } = getState();
-    const { busStopsMap, startStop, endStop } = map;
+    const { startStop, endStop } = map;
 
     dispatch({ type: types.ON_MAP_LOAD, google });
 
     if (startStop && endStop) {
-      dispatch(calculateRoute(map.graph, busStopsMap, startStop, endStop, google));
+      dispatch(calculateRoute(startStop, endStop));
     }
   };
 
@@ -110,24 +126,20 @@ export const adjacencyListLoaded = (graph, busStopsMap, busServices) => ({
   busServices,
 });
 
-// export const drawPolylines = (graph, busStopsMap, startStop, endStop) => {};
+export const selectStartStop = startStop =>
+  (dispatch) => {
+    dispatch(selectStartStopActions.success({ startStop }));
+  };
 
-
-// Bus stop actions
-export const selectStartStop = startStop => ({
-  type: types.SELECT_START_STOP,
-  startStop,
-});
-
-export const selectEndStop = endStop => ({
-  type: types.SELECT_END_STOP,
-  endStop,
-});
+export const selectEndStop = endStop =>
+  (dispatch) => {
+    dispatch(selectEndStopActions.success({ endStop }));
+  };
 
 export const selectStartEndStop = (start, end) =>
   (dispatch, getState) => {
     const { map } = getState();
-    const { busStopsMap, startStop, endStop, google } = map;
+    const { startStop, endStop, google } = map;
 
     if (start) {
       dispatch(selectStartStop(start));
@@ -138,6 +150,6 @@ export const selectStartEndStop = (start, end) =>
     }
 
     if (google && ((startStop && end) || (start && endStop) || (start && end))) {
-      dispatch(calculateRoute(map.graph, busStopsMap, start || startStop, end || endStop, google));
+      dispatch(calculateRoute(start || startStop, end || endStop));
     }
   };
---
+++ app/src/components/Polyline.js
@@ -0,0 +1,41 @@
+import { Component, PropTypes } from 'react';
+
+class Polyline extends Component {
+  componentWillMount() {
+    if (this.props.google) {
+      const polyline = new this.props.google.maps.Polyline({
+        map: this.props.google.map,
+        geodesic: true,
+        strokeColor: this.props.color,
+        strokeOpacity: 0.7,
+        strokeWeight: 3,
+        clickable: false,
+        path: this.props.routePath,
+      });
+      this.setState({ polyline });
+    }
+  }
+  componentWillUnmount() {
+    if (this.state && this.state.polyline) {
+      this.state.polyline.setMap(null);
+    }
+  }
+  render() {
+    this.state.polyline.setMap(this.props.google.map);
+    return null;
+  }
+}
+
+Polyline.defaultProps = {
+  google: null,
+  routePath: null,
+  color: '#000',
+};
+
+Polyline.propTypes = {
+  google: PropTypes.any,
+  routePath: PropTypes.any,
+  color: PropTypes.string,
+};
+
+export default Polyline;
